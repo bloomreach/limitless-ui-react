5e9a478704bcaeb3f55022fbb89841fa97e0b6e1
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2024-07-17 20:45:44 -0700

    docs(searchbox): Add explanatory comments

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,3 +1,11 @@
+/**
+ * Creates a debounced function that delays invoking the provided function until after the specified
+ * wait time has elapsed since the last time the debounced function was invoked.
+ *
+ * @param func - The function to debounce.
+ * @param wait - The number of milliseconds to delay.
+ * @returns A new debounced function.
+ */
 export function debounce<T extends (...args: any[]) => any>(
   func: T,
   wait: number,
@@ -9,6 +17,8 @@ export function debounce<T extends (...args: any[]) => any>(
       clearTimeout(timeout);
     }
 
+    // We apply 'this' to preserve the context when the debounced function is called.
+    // This is important for methods that rely on the 'this' keyword.
     timeout = setTimeout(() => {
       timeout = null;
       func.apply(this, args);
