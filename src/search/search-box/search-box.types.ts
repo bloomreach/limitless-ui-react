ba935201904a253210d7c1e1865da47e429f3e48
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2024-08-30 21:44:04 -0700

    fix: Remove redundant className property
    CSS Classes should be added through the `classNames` object

--- src/search/search-box/search-box.types.ts
+++ src/search/search-box/search-box.types.ts
@@ -3,11 +3,6 @@ import { PropsWithChildren } from 'react';
 
 export interface SearchBoxProps extends PropsWithChildren {
   /**
-   * Custom class name for the container of the component.
-   */
-  className?: string;
-
-  /**
    * The Configuration for creating a Bloomreach search integration
    */
   configuration: Configuration;
