1f9fbbb8a31cb87c9fc44ae5058df797a738d26c
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2024-11-18 13:16:48 -0800

    fix: Organize searchbox files similar to other compound components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export * from './swatch-bar'
 export * from './product-card';
 export * from './search-box';
-export * from './suggestions';
 export * from './theme';
 export * from './range';
 export * from './checkbox-group';
--- src/components/search-box/index.ts
+++ src/components/search-box/index.ts
@@ -1,3 +1,11 @@
-export { SearchBox } from './search-box';
+import { SearchBoxRoot } from './components/search-box-root';
+import { Suggestions } from './components/search-box-suggestions';
+
+export const SearchBox = {
+  Root: SearchBoxRoot,
+  Suggestions: Suggestions,
+};
+
+export type * from './search-box.types';
+
 export { useSearchBox } from '../../hooks/search-box.hook';
-export * from './search-box.types';
